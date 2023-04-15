810f456c46fd1f0a7f8fdcce0aa56913990a7acf
polixonrio <polixonrio@gmail.com>
2023-04-15 12:43:29 +0000

    already12

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -67,8 +67,8 @@ function toggleNavbar() {
 
 	<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-		<a href="" class="flex items-center">
-			<img src="images/logos.webp" class="h-8 mr-3" alt="Asterisc Logo">
+		<a href="/" class="flex items-center">
+			<img src="images/logos.webp"  class="h-8 mr-3" alt="Asterisc Logo">
 			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Asterisc</span>
 		</a>
 		<div class="flex md:order-2">
