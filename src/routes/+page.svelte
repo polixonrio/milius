08da3c7b67a32803e62d70af20c565cd5135eba0
polixonrio <polixonrio@gmail.com>
2023-04-15 13:08:13 +0000

    already21

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <main class="dark:bg-gray-800 bg-gray-100  dark:text-gray-100">
 	<div class="bg-gray flex items-center justify-center pt-12 h-screen">
 		<div class="relative isolate px-6 md:py-16 mx-auto align-middle ">
-			<div
+			<!-- <div
 				class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
 				aria-hidden="true"
 			>
@@ -14,7 +14,7 @@
 					class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
 					style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 				/>
-			</div>
+			</div> -->
 			<div class="mx-auto max-w-2xl align-middle">
 				<div class="hidden sm:mb-8 sm:flex sm:justify-center">
 					<div
