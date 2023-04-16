0ef46e077aaa3b7ff3050bd8ce4254b394291bb2
polixonrio <polixonrio@gmail.com>
2023-04-16 13:15:25 +0000

    swebpsc20837897987911fff19

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -85,7 +85,7 @@
 		<div class="items-center justify-between w-full md:flex md:w-auto md:order-1 text-center {showMenu ? '' : 'hidden'} " id="navbar-sticky">
 		  <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 			<li>
-			  <a href="/" on:click={()=>setActive(0)} class="block py-2 pl-3 pr-4 {active === 0 ? 'bg-purple-800 md:text-purple-800' : 'bg-transparent sm:text-'} sm:bg-transparent text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
+			  <a href="/" on:click={()=>setActive(0)} class="block py-2 pl-3 pr-4 {active === 0 ? 'bg-purple-800 md:text-purple-800' : 'bg-transparent'} sm:bg-transparent text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
 			</li>
 			<li>
 			  <a href="/aboutus"  on:click={()=>setActive(1)} class="block py-2 pl-3 pr-4 {active === 1 ? 'bg-purple-800 md:text-purple-800' : 'bg-transparent'} sm:bg-transparent text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-700 md:p-0 md:dark:hover:text-purple-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
