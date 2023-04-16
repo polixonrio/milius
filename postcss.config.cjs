f5fc619dad6abe69c87da07ae31e38dfc0b3dad3
polixonrio <polixonrio@gmail.com>
2023-04-16 11:57:47 +0000

    Iframesx1acnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,10 @@ const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require("cssnano")
 
+const mode = process.env.NODE_ENV
+const dev = mode === "development"
+
+
 
 const config = {
 	plugins: [
