266ba4df3867221a694b7c628b953d58eb978738
polixonrio <polixonrio@gmail.com>
2023-04-16 06:43:52 +0000

    revertion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -66,15 +66,7 @@
 					</div>
 				</div>
 			</div>
-			<!-- <div
-				class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-				aria-hidden="true"
-			>
-				<div
-					class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-					style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-				/>
-			</div> -->
+			
 		</div>
 	</div>
 
@@ -99,7 +91,7 @@
 							Web Development <br />
 
 							<a href="/courses"
-								><button class="border rounded px-4 hover:bg-purple-600 text-xl my-4"
+								><button class="border rounded px-3 hover:bg-purple-600 text-lg my-4"
 									>Learn Now</button
 								></a
 							>
@@ -194,7 +186,7 @@
 						</p>
 					</dd>
 				</div>
-				<Hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+
 				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
 					<dd
 						class="order-first text-3xl font-semibold tracking-tight dark:text-white text-gray-900 sm:text-5xl"
@@ -265,8 +257,8 @@
 			</div>
 		</div>
 	</div>
-	<Hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-	<div class="bg-white dark:bg-black py-14 sm:py-22">
+
+	<div class="bg-white dark:bg-black py-24 sm:py-32">
 		<div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
 			<div class="max-w-2xl">
 				<h2 class="text-3xl font-bold tracking-tight dark:text-white text-gray-900 sm:text-4xl">
@@ -276,7 +268,7 @@
 					We have an excellent team of Instructors to guide you.
 				</p>
 			</div>
-			<ul class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+			<ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
 				<li>
 					<div class="flex items-center gap-x-6">
 						<img class="h-16 w-16 rounded-full" src="/images/csir.webp" alt="" />
