af2c8a5e5e26c43ce2113d02cd35d64bd750a10d
polixonrio <polixonrio@gmail.com>
2023-04-16 13:02:52 +0000

    swebpsc208399

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -235,6 +235,60 @@
 		</div>
 	</div>
 
+	<div class="bg-white dark:bg-black py-16 sm:py-16 ">
+		<div class="mx-auto max-w-7xl px-6 lg:px-8">
+			<p class="text-center font-bold  md:text-6xl text-2xl mb-10">
+				LEARN <span class=" text-purple-600 "> MORE </span>NOW
+			</p>
+			<dl class="grid grid-cols-1 gap-x-8 gap-y-6 text-center lg:grid-cols-2">
+				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
+					<dd
+						class="order-first text-3xl font-semibold tracking-tight dark:text-white text-gray-900 sm:text-5xl"
+					>
+						<img
+							class="col-span-2 rounded  mx-auto   w-22  object-contain lg:col-span-1"
+							src="images/webdev.webp"
+							alt="Transistor"
+							width="158"
+							height="48"
+						/>
+						<p class="text-center font-bold  md:text-5xl text-xl mb-10">
+							Web Development <br />
+
+							<LearnMore />
+						</p>
+					</dd>
+				</div>
+
+			
+
+				
+
+	
+
+
+				<div class="mx-auto flex max-w-xs flex-col gap-y-4">
+					<dd
+						class="order-first text-3xl font-semibold tracking-tight dark:text-white text-gray-900 sm:text-5xl"
+					>
+						<img
+							class="col-span-2  mx-auto  rounded w-22 object-contain lg:col-span-1"
+							src="images/ml.webp"
+							alt="Transistor"
+							width="158"
+							height="48"
+						/>
+						<p class="text-center font-bold  md:text-5xl text-xl mb-10">
+							Machine Learning <br />
+							<LearnMore />
+						</p>
+					</dd>
+				</div>
+			</dl>
+		</div>
+	</div>
+
+
 	<div class="bg-white dark:bg-black py-24 sm:py-32">
 		<div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
 			<div class="max-w-2xl">
@@ -248,7 +302,7 @@
 			<ul  class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
 				<li>
 					<div class="flex items-center gap-x-6">
-						<img class="h-16 w-16 rounded-full" src="/images/csir.webp" alt="" />
+						<img class="h-26 w-26 rounded-full" src="/images/csir.webp" alt="" />
 						<div class="">
 							<h3
 								class="text-xl  font-semibold  leading-7 tracking-tight dark:text-white text-gray-900"
@@ -262,7 +316,7 @@
 
 				<li>
 					<div class="flex items-center gap-x-6">
-						<img class="h-16 w-16 rounded-full" src="/images/hsir.webp" alt="" />
+						<img class="h-26 w-26 rounded-full" src="/images/hsir.webp" alt="" />
 						<div>
 							<h3
 								class="text-xl font-semibold leading-7 tracking-tight dark:text-white text-gray-900"
@@ -276,7 +330,7 @@
 
 				<li>
 					<div class="flex items-center gap-x-6">
-						<img class="h-16 w-16 rounded-full" src="/images/pmaam.webp" alt="" />
+						<img class="h-26 w-26 rounded-full" src="/images/pmaam.webp" alt="" />
 						<div>
 							<h3
 								class="text-xl font-semibold leading-7 tracking-tight dark:text-white text-gray-900"
@@ -290,7 +344,7 @@
 
 				<li>
 					<div class="flex items-center gap-x-6">
-						<img class="h-16 w-16 rounded-full" src="/images/ssir.webp" alt="" />
+						<img class="h-26 w-26 rounded-full" src="/images/ssir.webp" alt="" />
 						<div>
 							<h3
 								class="text-xl font-semibold leading-7 tracking-tight dark:text-white text-gray-900"
