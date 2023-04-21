9d835f55970712919b2df95ff16def13478059e6
xilihulu <xilihulu@pm.me>
2023-04-22 01:45:10 +0530

    changes2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	export const prerender = true;
+
 	import '../app.postcss';
 	import { onMount } from 'svelte';
 	import {
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,7 +24,7 @@ const config = {
         'image-one':
           "url('/images/whaite.webp')",
         'image-two':
-          "url('/images/bri.webp')",
+          "url('/images/ver.svg')",
       }),
       colors: {
         primary: {"50":"#fff1f2","100":"#ffe4e6","200":"#fecdd3","300":"#fda4af","400":"#fb7185","500":"#f43f5e","600":"#e11d48","700":"#be123c","800":"#9f1239","900":"#881337"}
