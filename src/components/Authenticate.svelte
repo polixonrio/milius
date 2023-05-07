d58e921fa4c351d52e2f6ae58017cda3a17c8e63
polixonrio <polixonrio@gmail.com>
2023-05-07 10:32:27 +0000

    initia

---
+++ src/components/Authenticate.svelte
@@ -0,0 +1,42 @@
+<div class="authContainer" >
+
+    <form action="">
+
+        <h1>Login</h1>
+        
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+        <label>
+            <input type="email" placeholder="email" />
+        </label>
+    </form>
+</div>
+
+<style>
+
+    .authContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+        padding: 24px;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        width: 400px;
+        max-width: 100%;
+        margin: 0 auto;
+    }
+    
+    form input {
+        width: 100px;
+    }
+
+</style>
