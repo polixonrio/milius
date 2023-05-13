e1c795e9593b55d1a52cd56bca804305006c715f
polixonrio <polixonrio@gmail.com>
2023-05-13 11:54:17 +0000

    ol114555

---
+++ src/routes/wc/+page.svelte
@@ -0,0 +1,33 @@
+<div
+  class="vscode activeShadow"
+
+>
+
+  <div class="mainApp">
+    <iframe
+      title="VS Code"
+      src="https://stackblitz.com/github/yashash-pugalia/win11Svelte?embed=1&file=src/App.svelte"
+      frameborder="0"
+    />
+  </div>
+</div>
+
+<style>
+  .vscode {
+    background: #202327;
+    position: absolute;
+    inset: 10%;
+    border-radius: 8px;
+    overflow: hidden;
+    resize: both;
+  }
+  .mainApp {
+    display: flex;
+    position: absolute;
+    width: 100%;
+    height: calc(100% - 36px);
+  }
+  iframe {
+    flex-grow: 1;
+  }
+</style>
