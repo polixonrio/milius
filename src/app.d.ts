deb8fadce4bc95066d8be591a4d670af231693dc
polixonrio <polixonrio@gmail.com>
2023-05-13 11:19:37 +0000

    inital lucia

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,14 +1,14 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
+/// <reference types="lucia-auth" />
+declare namespace Lucia {
+	type Auth = import('$lib/server/lucia.js').Auth;
+	type UserAttributes = {
+		username: string;
+	};
+}
+
+/// <reference types="@sveltejs/kit" />
 declare namespace App {
-  interface Locals {
-    userid: string;
-  }
-
-  // interface PageData {}
-
-  // interface Error {}
-
-  // interface Platform {}
+	interface Locals {
+		auth: import('lucia-auth').AuthRequest;
+	}
 }
---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,10 @@
+
+
+<svelte:head>
+	<title>Lucia demo</title>
+</svelte:head>
+<div class="mt-12">
+	<h1><a href="/">Lucia + SvelteKit demo</a></h1>
+	<slot />
+</div>
+
