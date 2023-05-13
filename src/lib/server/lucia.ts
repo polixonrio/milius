4bf25bf2759cf45dcc91e5f0dfee96fb3fce1dae
polixonrio <polixonrio@gmail.com>
2023-05-13 11:34:04 +0000

    ol

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -5,7 +5,7 @@ import { dev } from '$app/environment';
 import { PrismaClient } from '@prisma/client';
 
 import { github } from '@lucia-auth/oauth/providers';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+// import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = lucia({
 	adapter: prisma(new PrismaClient()),
@@ -19,9 +19,9 @@ export const auth = lucia({
 	middleware: sveltekit()
 });
 
-export const githubAuth = github(auth, {
-	clientId: GITHUB_CLIENT_ID,
-	clientSecret: GITHUB_CLIENT_SECRET
-});
+// export const githubAuth = github(auth, {
+// 	clientId: GITHUB_CLIENT_ID,
+// 	clientSecret: GITHUB_CLIENT_SECRET
+// });
 
 export type Auth = typeof auth;
