078a85f802136dac110c064ed40181c1b8795756
polixonrio <polixonrio@gmail.com>
2023-05-13 12:46:58 +0000

    lm1

--- src/routes/wc/+page.svelte
+++ src/routes/wc/+page.svelte
@@ -11,12 +11,7 @@
     />
   </div>
 </div>
-<iframe
-      title="VS Code"
-      src="https://stackblitz.com/github/yashash-pugalia/win11Svelte?embed=1&file=src/App.svelte"
-      frameborder="0"
-    />
-  </div>
+
 <style>
   .vscode {
     background: #202327;
