ec87c31da14882420c7c40cf5111380b8046891d
polixonrio <polixonrio@gmail.com>
2023-05-13 13:05:33 +0000

    oofe

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -21,7 +21,7 @@
 </section>-->
 
 <section class="bg-gray-50 dark:bg-gray-900">
-	<div class="flex flex-col mt-4 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+	<div class="flex flex-col mt-12 items-center justify-center px-6 py-8 mx-auto sm:h-screen lg:py-0">
 		<a href="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
 			<img src="images/logo.webp" alt="logo" />
 		</a>
