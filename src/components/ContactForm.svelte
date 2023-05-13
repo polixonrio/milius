dd916198e77f260c6977ca495c9c3eadc0e14355
polixonrio <polixonrio@gmail.com>
2023-05-13 13:29:22 +0000

    inu

---
+++ src/components/ContactForm.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+	import { onMount } from 'svelte/types/runtime/internal/lifecycle';
+
+	onMount(() => {
+		const form = document.getElementById('sheetdb-form'));
+
+		if (form !== undefined) {
+			// continue - error suppressed when used in this way.
+
+			form.addEventListener('submit', (e) => {
+				e.preventDefault();
+				fetch(form.action, {
+					method: 'POST',
+					body: new FormData(document.getElementById('sheetdb-form'))
+				})
+					.then((response) => response.json())
+					.then((html) => {
+						// you can put any JS code here
+						alert('success');
+					});
+			});
+		}
+	});
+</script>
+
+<form
+	method="POST"
+	action="https://script.google.com/macros/s/AKfycby7oJo_s1gF8s7Y3SHWlxL5UodDj7CH6UiAy8vSOnPp85Gd8_MogoBXA63SeLqeAZfW/exec"
+	class="space-y-8"
+	id="sheetdb-form"
+>
+	<div>
+		<label for="Name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+			>Your Name</label
+		>
+		<input
+			type="text"
+			id="Name"
+			name="Name"
+			class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+			placeholder="Jane Doe"
+			required
+		/>
+	</div>
+	<div>
+		<label for="Email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+			>Your email</label
+		>
+		<input
+			type="email"
+			id="Email"
+			name="Email"
+			class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+			placeholder="name@asterisc.in"
+			required
+		/>
+	</div>
+	<div>
+		<label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+			>Subject</label
+		>
+		<input
+			type="text"
+			id="subject"
+			name="Subject"
+			class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+			placeholder="Let us know how we can help you"
+			required
+		/>
+	</div>
+	<div class="sm:col-span-2">
+		<label for="Mobile" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+			>Your Phone</label
+		>
+		<input
+			id="Mobile"
+			name="Mobile"
+			class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+			placeholder="Mobile Number"
+		/>
+	</div>
+	<div class="sm:col-span-2">
+		<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+			>Your message</label
+		>
+		<input
+			id="message"
+			name="Message"
+			class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+			placeholder="Leave a comment..."
+		/>
+	</div>
+
+	<button
+		type="submit"
+		class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+		>Send message</button
+	>
+</form>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -90,7 +90,7 @@
 							aria-current="page">Home</a
 						>
 					</li>
-					<!-- <li>
+					<li>
 						<a
 							href="/aboutus"
 							on:click={() => setActive(1)}
@@ -109,7 +109,7 @@
 								: 'bg-transparent text-purple-400'} lg:bg-transparent hover:border-black-800 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-purple-700 lg:p-0 lg:dark:hover:text-purple-500 dark:text-purple-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
 							>Testimonials</a
 						>
-					</li> -->
+					</li>
 					<li>
 						<a
 							href="/contactus"
@@ -119,26 +119,6 @@
 								: 'bg-transparent text-purple-400'} lg:bg-transparent hover:border-black-800 text-gray-900 rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-purple-700 lg:p-0 lg:dark:hover:text-purple-500 dark:text-purple-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
 							>Contact Us</a
 						>
-					</li>
-					<li>
-						<button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg class="w-5 h-5 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></button>
-						<!-- Dropdown menu -->
-						<div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-							<ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-							  <li>
-								<a href="/aboutus" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-							  </li>
-							  <li>
-								<a href="/faqs" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-							  </li>
-							  <li>
-								<a href="/testimonials" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-							  </li>
-							</ul>
-							<!-- <div class="py-1">
-							  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">Sign out</a>
-							</div> -->
-						</div>
 					</li>
 					<li>
 						<a
